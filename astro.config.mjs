450ecb15248a80b7e7c8560acdc495a0ae9f31ad
sbcar01 <sergio.barria@oceanscode.com>
2024-04-19 15:41:38 -0500

    update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({ applyBaseStyles: false }), react()],
-    output: "hybrid"
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }),
+    react(),
+    sitemap(),
+    robotsTxt()
+  ],
+  output: "hybrid"
 });

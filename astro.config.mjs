9c578d8310a9f25f64bb7ed557fc5c3d28f65a19
sbcar01 <sergio.barria@oceanscode.com>
2024-09-10 21:58:29 -0500

    update the astro site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-    // site: "https://beachmanagementnosara.com",
-    site: "https://bmn-new.netlify.app",
+    site: "https://beachmanagementnosara.com",
     integrations: [
         tailwind({
             applyBaseStyles: false

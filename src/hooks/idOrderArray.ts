c0e4e33d9ad8677553233d7a76086f1e43a256f7
sbcar01 <sergio.barria@oceanscode.com>
2024-11-19 13:50:19 -0500

    re-order properties

--- src/hooks/idOrderArray.ts
+++ src/hooks/idOrderArray.ts
@@ -2,38 +2,36 @@ export const idsInOrder = [
     368123, // Tres Hermanas, Luxury home
     280439, // Nai'a Luxury Vacation House
     277894, // Shanta Pura
-    439129, // Casa Los Pochotes, Luxury Vacation Home (New Listing
-    466024, // Terrace House, Luxury Vacation Home (New Listing
+    439129, // Casa Los Pochotes, Luxury Vacation Home (New Listing)
+    466024, // Terrace House, Luxury Vacation Home (New Listing)
     279982, // Almendra
-    481099, // Calma House, Luxury Vacation Home (New Listing
+    481099, // Calma House, Luxury Vacation Home (New Listing)
     617958, // Ocean Top, Walk to Surfing Beach
-    483495, // Utopia Escapista
     576665, // Free Spirits House
     530996, // Ansui House, Vacation Home
     282029, // Villa Belitza
+    621252, // Haven
     277622, // Hermandad
-    530060, // Last PC Luxury Vacation Home
-    570825, // Villa Paz Shalom, New Listing
-    482720, // Verano Eterno
+    482720, // Verano
     277581, // FLOW - Main House, Vacation Home
     277588, // Flow - Casita, Vacation Home
-    277603, // FLOW -  Villa O, Vacation Home
-    277594, // FLOW -  Villa W, Vacation Home
+    277603, // FLOW - Villa O, Vacation Home
+    277594, // FLOW - Villa W, Vacation Home
     292658, // FLOW 4 Villas, Luxury Vacation Homes
     292644, // FLOW MainHouse + Casita
     288487, // El NIDO 4 Bedroom, 4 Bathroom Luxury House
     277892, // Casa Sierra
-    351551, // Ashaya, Luxury Vacation Home
     277727, // Robins Nest 3 Bedroom, 2 Bathroom Oasis
     342716, // Alma del Mar
-    334483, // Casa Catalina, Vacation Home
     613808, // Casa DosSantos
-    531664, // JOYA: A Modern Jungle Getaway near the Beach
-    531672, // Casita IVY
     539969, // Casa Calmate
-    277895, // Casa Paco Main House, long term rental available
-    581304, // Casa Chiara
+    531664, // JOYA: A Modern Jungle Getaway near the Beach
     581332, // Casa Daria
+    531672, // Casita IVY
+    581304, // Casa Chiara
+    570825, // Villa Paz Shalom
+    334483, // Casa Catalina, Vacation Home
+    277895, // Casa Paco Main House, long term rental available
     343324 // Paco Studio
 ];
 
@@ -41,4 +39,3 @@ export const videoUrls: Record<number, string> = {
     439129: "https://www.youtube.com/embed/dHTySH3ky-U?si=SCojby0hCEXsPKI5"
     // Add other property IDs and their video URLs here...
 };
-
